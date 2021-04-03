12134737b2fae5ebff044fd6f58bf9633ad256d6
Edilton <ediltondanniken@gmail.com>
2021-04-03 15:40:28 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Edilton Danniken on 03/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
